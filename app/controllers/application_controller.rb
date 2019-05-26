f7cb32c5f8a519ea6a4aa733157968b66a2e35dc
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-05-27 08:47:40 +1200

    add bank sync

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
   protect_from_forgery prepend: true
+  breadcrumb 'Home', :root
 end
