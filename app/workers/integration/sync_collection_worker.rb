a036f760fd7c7ca34e8960820f6a3a9d3c8af013
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-10-31 09:41:28 +1300

    change later to async

--- app/workers/integration/sync_collection_worker.rb
+++ app/workers/integration/sync_collection_worker.rb
@@ -1,7 +1,7 @@
 class Integration::SyncCollectionWorker < ApplicationWorker
   def perform
     Integration.where(locked_at: nil).find_each do |integration|
-      Integration::SyncWorker.perform_later(integration.id)
+      Integration::SyncWorker.perform_async(integration.id)
     end
   end
 end
